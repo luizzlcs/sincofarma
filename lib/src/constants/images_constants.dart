338bfe2b838946021d65d701b65d84dba3d6a5ec
Luizzlcs <luizzlcs@gmail.com>
2024-04-25 17:20:37 -0300

    Configurado constants de imagens para o App.

---
+++ lib/src/constants/images_constants.dart
@@ -0,0 +1,32 @@
+abstract class ImagesConstants{
+  static const logoCurso = 'assets/images/logo_cursos.png';
+  static const logoCursoWhite = 'assets/images/logo_cursos_white.png';
+  static const add = 'assets/images/add.png';
+  static const arrow = 'assets/images/arrow.png';
+  static const auriculotherapy = 'assets/images/auriculotherapy.png';
+  static const block = 'assets/images/block.png';
+  static const cardCod = 'assets/images/card_cod.png';
+  static const check = 'assets/images/check.png';
+  static const cnpj = 'assets/images/cnpj.png';
+  static const cpf = 'assets/images/cpf.png';
+  static const credCard = 'assets/images/cred_card.png';
+  static const email = 'assets/images/email.png';
+  static const eye = 'assets/images/eye.png';
+  static const firstAid = 'assets/images/first_aid.png';
+  static const flagsCards = 'assets/images/flags_cards.png';
+  static const group = 'assets/images/group.png';
+  static const heart = 'assets/images/heart.png';
+  static const houer = 'assets/images/houer.png';
+  static const info = 'assets/images/info.png';
+  static const injectable = 'assets/images/injectable.png';
+  static const logoPix = 'assets/images/logo_pix.png';
+  static const map = 'assets/images/map.png';
+  static const notification = 'assets/images/notification.png';
+  static const peopleName = 'assets/images/people_name.png';
+  static const profile = 'assets/images/profile.png';
+  static const search = 'assets/images/search.png';
+  static const student = 'assets/images/student.png';
+  static const userProfile = 'assets/images/user_profile.png';
+  static const vaccination = 'assets/images/vaccination.png';
+  static const whatsApp = 'assets/images/whatsapp.png';
+}

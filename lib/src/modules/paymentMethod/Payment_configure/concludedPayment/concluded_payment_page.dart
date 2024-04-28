954ec92eb0cc47143a9aaa04619bffdecca184f1
Luizzlcs <luizzlcs@gmail.com>
2024-04-28 20:47:53 -0300

    Criado página do módulo de pagamento.

---
+++ lib/src/modules/paymentMethod/Payment_configure/concludedPayment/concluded_payment_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ConcludedPaymentPage extends StatelessWidget {
+
+  const ConcludedPaymentPage({ super.key });
+
+   @override
+   Widget build(BuildContext context) {
+       return Scaffold(
+           appBar: AppBar(title: const Text('Pagamento concluído'),),
+           body: Container(),
+       );
+  }
+}

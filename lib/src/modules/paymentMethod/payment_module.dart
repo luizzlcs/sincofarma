07752bdcc844758e620eb4a7997a4c6419ff9369
Luizzlcs <luizzlcs@gmail.com>
2024-04-28 20:43:29 -0300

    Criado o módulo de pagamento

---
+++ lib/src/modules/paymentMethod/payment_module.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+
+class PaymentModule extends FlutterGetItModule {
+  @override
+  
+  String get moduleRouteName => '/payment';
+
+  @override
+ 
+  Map<String, WidgetBuilder> get pages => {};
+  
+}

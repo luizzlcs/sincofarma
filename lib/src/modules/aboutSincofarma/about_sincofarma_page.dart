38a2cae2acd3a9a69b32128761d7ffd70bc616f7
Luizzlcs <luizzlcs@gmail.com>
2024-04-28 20:41:17 -0300

    Criando página sobre o Sincofarma.

---
+++ lib/src/modules/aboutSincofarma/about_sincofarma_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AboutSincofarmaPage extends StatelessWidget {
+
+  const AboutSincofarmaPage({ super.key });
+
+   @override
+   Widget build(BuildContext context) {
+       return Scaffold(
+           appBar: AppBar(title: const Text('Sobre o Sincofarma'),),
+           body: Container(),
+       );
+  }
+}

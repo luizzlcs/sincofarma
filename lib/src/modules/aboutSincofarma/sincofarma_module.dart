a61731bf12351c0fc9cd883e8190d8323400a9ad
Luizzlcs <luizzlcs@gmail.com>
2024-04-28 20:41:42 -0300

    Criado módulo Sincofarma

---
+++ lib/src/modules/aboutSincofarma/sincofarma_module.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+
+class SincofarmaModule extends FlutterGetItModule {
+  @override
+ 
+  String get moduleRouteName => '/sincofarma';
+
+  @override
+  
+  Map<String, WidgetBuilder> get pages => {};
+  
+}

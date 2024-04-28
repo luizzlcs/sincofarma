44a1615493d44967a2dc4eac482dfe7f64811ee8
Luizzlcs <luizzlcs@gmail.com>
2024-04-28 20:44:03 -0300

    Criado páginas rotas do módulo de pagamento.

---
+++ lib/src/modules/paymentMethod/PaymentMethodPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class RegisterCoursePage extends StatelessWidget {
+
+  const RegisterCoursePage({ super.key });
+
+   @override
+   Widget build(BuildContext context) {
+       return Scaffold(
+           appBar: AppBar(title: const Text('Selecione a forma de pagamento'),),
+           body: Container(),
+       );
+  }
+}
---
+++ lib/src/modules/paymentMethod/Payment_configure/payment_configure_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PaymentConfigurePage extends StatelessWidget {
+
+  const PaymentConfigurePage({ super.key });
+
+   @override
+   Widget build(BuildContext context) {
+       return Scaffold(
+           appBar: AppBar(title: const Text('Selecionar tipo de cliente'),),
+           body: Container(),
+       );
+  }
+}

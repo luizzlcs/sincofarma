cd4166aae2fb29532bc042a02b5b6c96ad622753
Luizzlcs <luizzlcs@gmail.com>
2024-04-28 20:42:22 -0300

    Ajustando as rotas do módulo Auth

--- lib/src/modules/auth/auth_module.dart
+++ lib/src/modules/auth/auth_module.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_getit/flutter_getit.dart';
-
-
+import '../home/nextCourses/next_courses_page.dart';
 import 'Register/register_router.dart';
 import 'login/login_router.dart';
 
 class AuthModule extends FlutterGetItModule {
   @override
-  List<Bind<Object>> get bindings => [
-    
-    
-  ];
+  List<Bind<Object>> get bindings => [];
 
   @override
   String get moduleRouteName => '/auth';
@@ -19,5 +15,6 @@ class AuthModule extends FlutterGetItModule {
   Map<String, WidgetBuilder> get pages => {
         '/login': (_) => const LoginRouter(),
         '/register': (_) => const RegisterRouter(),
+        '/home':(_) =>  const NextCoursesPage(),
       };
 }

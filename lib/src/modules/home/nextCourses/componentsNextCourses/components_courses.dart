91b4692332d55a57f217e3267e7eb38368c695f6
Luizzlcs <luizzlcs@gmail.com>
2024-04-29 19:32:11 -0300

    Criando componetes da página de cursos.

--- lib/src/modules/home/nextCourses/componentsNextCourses/components_courses.dart
+++ lib/src/modules/home/nextCourses/componentsNextCourses/components_courses.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../../../../constants/images_constants.dart';
 import '../../../../theme/sincofarma_theme.dart';
 
 class ComponentsCourses extends StatelessWidget {
@@ -25,19 +24,56 @@ class ComponentsCourses extends StatelessWidget {
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            width: sizeOf.width * 0.35,
-            height: 130,
-            decoration: const BoxDecoration(
-              color: Colors.deepPurple,
-              borderRadius: BorderRadius.all(
-                Radius.circular(16),
+          Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Container(
+                width: sizeOf.width * 0.35,
+                height: 130,
+                decoration: const BoxDecoration(
+                  color: Colors.deepPurple,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: Image.asset(
+                    pathImage,
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
-            ),
-            child: Image.asset(
-              ImagesConstants.vaccination,
-              fit: BoxFit.cover,
-            ),
+              Container(
+                alignment: Alignment.center,
+                width: sizeOf.width * 0.24,
+                height: 31,
+                decoration: BoxDecoration(
+                  color: SincofarmaTheme.blueColor,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      starDate,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Roboto',
+                        fontSize: 10,
+                      ),
+                    ),
+                    const Text(
+                      '(Terça)',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Roboto',
+                        fontSize: 7,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
           Padding(
             padding: const EdgeInsets.only(left: 13.74),
@@ -51,26 +87,34 @@ class ComponentsCourses extends StatelessWidget {
                     style: const TextStyle(
                         fontSize: 24,
                         fontFamily: 'Roboto',
-                        fontWeight: FontWeight.w600),
+                        fontWeight: FontWeight.w600,
+                        height: 0.8),
                   ),
-                  Text(
-                    description,
-                    style: SincofarmaTheme.subTitleDecriptionRoboto.copyWith(
-                      overflow: TextOverflow.clip,
-                      height: 1.0,
+                  Padding(
+                    padding: const EdgeInsets.only(top: 4.0),
+                    child: Text(
+                      description,
+                      style: SincofarmaTheme.subTitleDecriptionRoboto.copyWith(
+                        overflow: TextOverflow.clip,
+                        height: 1.0,
+                      ),
                     ),
                   ),
                   Container(
                     margin: const EdgeInsets.only(top: 8),
                     alignment: Alignment.topLeft,
-                    width: 120,
+                    width: sizeOf.width * 0.30,
                     height: 20,
                     child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          textStyle: const TextStyle(
-                              fontSize: 14, fontFamily: 'Roboto')),
+                      style: ElevatedButton.styleFrom(),
                       onPressed: () {},
-                      child: const Text('Saiba mais'),
+                      child: const Text(
+                        'Saiba mais',
+                        style: TextStyle(
+                          fontFamily: 'Roboto',
+                          fontSize: 14,
+                        ),
+                      ),
                     ),
                   )
                 ],

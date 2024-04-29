5a042dd1eb4e8d082f3cfa6295154f4490b9b98e
Luizzlcs <luizzlcs@gmail.com>
2024-04-29 19:31:06 -0300

    Corrigido bug de OverFlower.

--- lib/src/modules/home/nextCourses/next_courses_page.dart
+++ lib/src/modules/home/nextCourses/next_courses_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+import 'package:sincofarma/src/model/mock/dummy_data.dart';
+import 'package:sincofarma/src/model/mock/dummy_model.dart';
 import 'package:sincofarma/src/modules/home/nextCourses/widgets/nex_courses_app_bar.dart';
 import 'package:sincofarma/src/theme/sincofarma_theme.dart';
 
@@ -14,27 +18,28 @@ class NextCoursesPage extends StatelessWidget {
     return Scaffold(
       body: SizedBox(
         width: sizeOf.width * 6,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const NextCoursesAppBar(),
-            const SizedBox(
-              height: 40,
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 10.0),
-              child: Text(
-                'Próximos Cursos',
-                style: SincofarmaTheme.titleStyle,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const NextCoursesAppBar(),
+              const SizedBox(
+                height: 40,
               ),
-            ),
-            
-            SizedBox(
-              width: sizeOf.width * 1.1,
-              height: sizeOf.width * 1.46,
-              child: const ListViewCourses(),
-            )
-          ],
+              const Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: Text(
+                  'Próximos cursos',
+                  style: SincofarmaTheme.titleStyle,
+                ),
+              ),
+              SizedBox(
+                width: sizeOf.width * 1.2,
+                height: sizeOf.width * 1.6,
+                child: const ListViewCourses(),
+              )
+            ],
+          ),
         ),
       ),
     );

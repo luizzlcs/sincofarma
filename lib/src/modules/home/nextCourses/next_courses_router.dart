aa1849b1d3fc4b80e81705f32ddaebf5dce9942d
Luizzlcs <luizzlcs@gmail.com>
2024-04-29 19:31:31 -0300

    criado instacia dos dados.

--- lib/src/modules/home/nextCourses/next_courses_router.dart
+++ lib/src/modules/home/nextCourses/next_courses_router.dart
@@ -3,12 +3,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_getit/flutter_getit.dart';
 import 'package:sincofarma/src/modules/home/nextCourses/next_courses_page.dart';
 
+import '../../../model/mock/dummy_data.dart';
+
 class NextCoursesRouter extends FlutterGetItModulePageRouter{
   const NextCoursesRouter({super.key});
 
   @override
   
-  List<Bind<Object>> get bindings => [];
+  List<Bind<Object>> get bindings => [
+    Bind.lazySingleton((i) => DummyData())
+  ];
 
   @override 
   WidgetBuilder get view => (BuildContext context)=> const NextCoursesPage();

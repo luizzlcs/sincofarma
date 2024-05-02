9e704e0dadcb59bbb1c1d4428d0e5cd32cd347ea
Luizzlcs <luizzlcs@gmail.com>
2024-05-02 01:38:09 -0300

    Criado página de métodos de pagamentos.

---
+++ lib/src/modules/paymentMethod/payment_method_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PaymentMethodPage extends StatelessWidget {
+
+  const PaymentMethodPage({ super.key });
+
+   @override
+   Widget build(BuildContext context) {
+       return Scaffold(
+           appBar: AppBar(title: const Text('Selecione a forma de pagamento'),),
+           body: Container(),
+       );
+  }
+}

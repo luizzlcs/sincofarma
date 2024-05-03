364ac886a35be022849c30df57db63a9a6ba98e3
Luizzlcs <luizzlcs@gmail.com>
2024-05-03 01:51:19 -0300

    Configurado nova rota de página

--- lib/src/modules/home/home_module.dart
+++ lib/src/modules/home/home_module.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_getit/flutter_getit.dart';
+import 'package:sincofarma/src/modules/home/nextCourses/aboutCourse/about_course_route.dart';
 import 'nextCourses/next_courses_router.dart';
 
 class HomeModule extends FlutterGetItModule {
@@ -12,5 +13,6 @@ class HomeModule extends FlutterGetItModule {
   @override
   Map<String, WidgetBuilder> get pages => {
         '/next-courses': (_) => const NextCoursesRouter(),
+        '/about-course': (context) => const AboutCourseRoute(),
       };
 }

b097f005b2c7ea46ffe90199038423e5751a5220
Luizzlcs <luizzlcs@gmail.com>
2024-05-04 22:24:20 -0300

    Ajustes

--- lib/src/model/mock/dummy_model.dart
+++ lib/src/model/mock/dummy_model.dart
@@ -1,13 +1,26 @@
+
 class DummyModel {
   DummyModel({
     required this.course,
     required this.description,
     required this.imagePath,
     required this.startDate,
+    required this.info,
+    required this.objective,
+    required this.amounts,
+    required this.theoryAndTechnique,
+    required this.local,
+    
   });
 
   final String course;
   final String description;
   final String imagePath;
   final String startDate;
+  final String info;
+  final String objective;
+  final String amounts;
+  final String theoryAndTechnique;
+  final String local;
+  
 }

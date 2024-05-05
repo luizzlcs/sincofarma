e5c1fbc42d42a61c7d4797798d0281301c5a9c5f
Luizzlcs <luizzlcs@gmail.com>
2024-05-04 22:23:56 -0300

    Ajustes

--- lib/src/model/mock/dummy_data.dart
+++ lib/src/model/mock/dummy_data.dart
@@ -9,6 +9,14 @@ class DummyData {
           'Programa de Formação e Habilitação de Vacinação para Farmacêuticos, visa capacitar profissionais da área para atuarem como vacinadores. Atende todas as exigências do CFF e Mec.',
       imagePath: ImagesConstants.vaccination,
       startDate: '01 e 02 de Março',
+      info:
+          'Lorem ipsum dolor sit amet. Est tempora animi aut incidunt dignissimos quo quia odit et iste excepturi qui repudiandae tempore. Ut fugiat dolorem qui dicta unde eos excepturi omnis est voluptatibus aperiam qui exercitationem sunt sit ipsum repellat qui quae blanditiis.',
+      objective:
+          'Lorem ipsum dolor sit amet. Et fugit reprehenderit ut quisquam similique aut velit quas et quia nesciunt. Aut labore veritatis in doloremque sunt et ipsam quas qui magnam reprehenderit et ducimus voluptatem ad magnam incidunt.',
+      amounts: 'R\$ 890,00',
+      theoryAndTechnique:
+          'Lorem ipsum dolor sit amet. Quo asperiores quod et veritatis vero sit accusantium alias est omnis ratione aut voluptas sint nam galisum adipisci? Sed vitae quia At rerum itaque et rerum similique e a voluptatum optio rem assumenda error.',
+      local: 'Av. da Lamedas SP - São Paulo',
     ),
     DummyModel(
       course: 'Primeiros Socorros',
@@ -16,6 +24,14 @@ class DummyData {
           'Capacitar o profissional que trabalha no atendimento de farmácia/drogaria, para que possa ter noções, segurança e muitas vezes percepção antecipada.',
       imagePath: ImagesConstants.firstAid,
       startDate: '11 de Março ',
+      info:
+          'Lorem ipsum dolor sit amet. Est tempora animi aut incidunt dignissimos quo quia odit et iste excepturi qui repudiandae tempore. Ut fugiat dolorem qui dicta unde eos excepturi omnis est voluptatibus aperiam qui exercitationem sunt sit ipsum repellat qui quae blanditiis.',
+      objective:
+          'Lorem ipsum dolor sit amet. Et fugit reprehenderit ut quisquam similique aut velit quas et quia nesciunt. Aut labore veritatis in doloremque sunt et ipsam quas qui magnam reprehenderit et ducimus voluptatem ad magnam incidunt.',
+      amounts: 'R\$ 890,00',
+      theoryAndTechnique:
+          'Lorem ipsum dolor sit amet. Quo asperiores quod et veritatis vero sit accusantium alias est omnis ratione aut voluptas sint nam galisum adipisci? Sed vitae quia At rerum itaque et rerum similique e a voluptatum optio rem assumenda error.',
+      local: 'Av. da Lamedas SP - São Paulo',
     ),
     DummyModel(
       course: 'Injetáveis',
@@ -23,6 +39,14 @@ class DummyData {
           'Curso é destinado para profissionais da saúde, para a  capacitação das habilidades das Técnicas de Aplicação de Medicamentos, conforme as regulamentações da ANVISA.',
       imagePath: ImagesConstants.injectable,
       startDate: '09 de Março',
+      info:
+          'Lorem ipsum dolor sit amet. Est tempora animi aut incidunt dignissimos quo quia odit et iste excepturi qui repudiandae tempore. Ut fugiat dolorem qui dicta unde eos excepturi omnis est voluptatibus aperiam qui exercitationem sunt sit ipsum repellat qui quae blanditiis.',
+      objective:
+          'Lorem ipsum dolor sit amet. Et fugit reprehenderit ut quisquam similique aut velit quas et quia nesciunt. Aut labore veritatis in doloremque sunt et ipsam quas qui magnam reprehenderit et ducimus voluptatem ad magnam incidunt.',
+      amounts: 'R\$ 890,00',
+      theoryAndTechnique:
+          'Lorem ipsum dolor sit amet. Quo asperiores quod et veritatis vero sit accusantium alias est omnis ratione aut voluptas sint nam galisum adipisci? Sed vitae quia At rerum itaque et rerum similique e a voluptatum optio rem assumenda error.',
+      local: 'Av. da Lamedas SP - São Paulo',
     ),
     DummyModel(
       course: 'Auriculoterapia',
@@ -30,6 +54,14 @@ class DummyData {
           'Auriculoterapia é uma técnica terapêutica derivada da acupuntura, com origem na medicina tradicional chinesa, que tem como foco a estimulação de pontos específicos.',
       imagePath: ImagesConstants.auriculotherapy,
       startDate: '02 de Abril ',
+      info:
+          'Lorem ipsum dolor sit amet. Est tempora animi aut incidunt dignissimos quo quia odit et iste excepturi qui repudiandae tempore. Ut fugiat dolorem qui dicta unde eos excepturi omnis est voluptatibus aperiam qui exercitationem sunt sit ipsum repellat qui quae blanditiis.',
+      objective:
+          'Lorem ipsum dolor sit amet. Et fugit reprehenderit ut quisquam similique aut velit quas et quia nesciunt. Aut labore veritatis in doloremque sunt et ipsam quas qui magnam reprehenderit et ducimus voluptatem ad magnam incidunt.',
+      amounts: 'R\$ 890,00',
+      theoryAndTechnique:
+          'Lorem ipsum dolor sit amet. Quo asperiores quod et veritatis vero sit accusantium alias est omnis ratione aut voluptas sint nam galisum adipisci? Sed vitae quia At rerum itaque et rerum similique e a voluptatum optio rem assumenda error.',
+      local: 'Av. da Lamedas SP - São Paulo',
     ),
     DummyModel(
       course: 'Assistência Farmacêutica',
@@ -37,6 +69,14 @@ class DummyData {
           'É com enorme satisfação que anunciamos o lançamento do Curso de Assistência Farmacêutica da Unova! Um curso online gratuito que promete ser um divisor de águas na sua carreira profissional.',
       imagePath: ImagesConstants.pharmaceutical,
       startDate: '03 de Junho ',
+      info:
+          'Lorem ipsum dolor sit amet. Est tempora animi aut incidunt dignissimos quo quia odit et iste excepturi qui repudiandae tempore. Ut fugiat dolorem qui dicta unde eos excepturi omnis est voluptatibus aperiam qui exercitationem sunt sit ipsum repellat qui quae blanditiis.',
+      objective:
+          'Lorem ipsum dolor sit amet. Et fugit reprehenderit ut quisquam similique aut velit quas et quia nesciunt. Aut labore veritatis in doloremque sunt et ipsam quas qui magnam reprehenderit et ducimus voluptatem ad magnam incidunt.',
+      amounts: 'R\$ 890,00',
+      theoryAndTechnique:
+          'Lorem ipsum dolor sit amet. Quo asperiores quod et veritatis vero sit accusantium alias est omnis ratione aut voluptas sint nam galisum adipisci? Sed vitae quia At rerum itaque et rerum similique e a voluptatum optio rem assumenda error.',
+      local: 'Av. da Lamedas SP - São Paulo',
     ),
   ];
 }

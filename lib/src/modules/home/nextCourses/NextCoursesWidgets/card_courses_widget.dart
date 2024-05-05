1f5bfe1a1d8a7f93afad6cf1975b88c4baf2aefa
Luizzlcs <luizzlcs@gmail.com>
2024-05-04 22:32:55 -0300

    Criando card de curses par alistaView

---
+++ lib/src/modules/home/nextCourses/NextCoursesWidgets/card_courses_widget.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import '../../../../theme/sincofarma_theme.dart';
+
+class CardCoursesWidget extends StatelessWidget {
+  final String pathImage;
+  final String starDate;
+  final String course;
+  final String description;
+
+  const CardCoursesWidget({
+    super.key,
+    required this.pathImage,
+    required this.starDate,
+    required this.course,
+    required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final sizeOf = MediaQuery.of(context).size;
+    return SizedBox(
+      width: double.infinity,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Container(
+                width: sizeOf.width * 0.35,
+                height: 130,
+                decoration: const BoxDecoration(
+                  color: Colors.deepPurple,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: Image.asset(
+                    pathImage,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                width: sizeOf.width * 0.24,
+                height: 31,
+                decoration: BoxDecoration(
+                  color: SincofarmaTheme.blueColor,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      starDate,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Roboto',
+                        fontSize: 10,
+                      ),
+                    ),
+                    const Text(
+                      '(Terça)',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Roboto',
+                        fontSize: 7,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 13.74),
+            child: SizedBox(
+              width: sizeOf.width * 0.51,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    course,
+                    style: const TextStyle(
+                        fontSize: 24,
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.w600,
+                        height: 0.8),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 4.0),
+                    child: Text(
+                      description,
+                      style: SincofarmaTheme.subTitleDecriptionRoboto.copyWith(
+                        overflow: TextOverflow.clip,
+                        height: 1.0,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 8),
+                    alignment: Alignment.topLeft,
+                    width: 120,
+                    height: 30,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(),
+                      onPressed: () {
+                        Navigator.of(context)
+                            .pushNamed('/home/about-course');
+                      },
+                      child: const Text(
+                        'Saiba mais',
+                        style: TextStyle(
+                          fontFamily: 'Roboto',
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/src/modules/home/nextCourses/NextCoursesWidgets/list_view_courses_widget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+import 'package:signals_flutter/signals_flutter.dart';
+import 'package:sincofarma/src/modules/home/nextCourses/NextCoursesWidgets/card_courses_widget.dart';
+import 'package:sincofarma/src/modules/home/nextCourses/next_courses_controller.dart';
+
+class ListViewCoursesWidget extends StatefulWidget {
+  const ListViewCoursesWidget({super.key});
+
+  @override
+  State<ListViewCoursesWidget> createState() => _ListViewCoursesWidgetState();
+}
+
+class _ListViewCoursesWidgetState extends State<ListViewCoursesWidget> {
+  final controller = Injector.get<NextCoursesController>();
+
+  @override
+  void initState() {
+    
+    controller.allCourses;
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Watch((BuildContext context) {
+      return ListView(
+        padding: const EdgeInsets.only(bottom: 100),
+        children:  controller.filteredCourses.map((course) {
+                return ListTile(
+                  dense: true,
+                  title: CardCoursesWidget(
+                    pathImage: course.imagePath,
+                    starDate: course.startDate,
+                    course: course.course,
+                    description: course.description,
+                  ),
+                  // onTap: () {
+                  //   debugPrint('Clicou no curso de: ${course.course}');
+                  // },
+                );
+              }).toList(),
+      );
+    });
+  }
+}

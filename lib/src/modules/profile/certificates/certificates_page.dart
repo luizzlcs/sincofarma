758214f81f4fc28d7160a3c79916edf1f33050cc
Luizzlcs <luizzlcs@gmail.com>
2024-05-10 14:31:47 -0300

    Criado página de certificados.

--- lib/src/modules/profile/certificates/certificates_page.dart
+++ lib/src/modules/profile/certificates/certificates_page.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-import '../../../widgets/in_construction_widget.dart';
+import '../../../theme/sincofarma_theme.dart';
+import '../../home/nextCourses/widgets/custom_app_bar_widget.dart';
+import 'widgets.dart/certificates_list_view_widget.dart';
 
 class CertificatesPage extends StatelessWidget {
+  const CertificatesPage({super.key});
 
-  const CertificatesPage({ super.key });
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Certificados'),),
-           body: const Center(
-        child: InConstructionWidget(),
+  @override
+  Widget build(BuildContext context) {
+    final Size sizeOf = MediaQuery.sizeOf(context);
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          height: sizeOf.height,
+          child: Column(
+            children: [
+              const CustomAppBarWidget(),
+              const Padding(
+                padding: EdgeInsets.all(12.0),
+                child: Text(
+                  'Meus Certificados',
+                  style: SincofarmaTheme.titleStyle,
+                ),
+              ),
+              SizedBox(
+                height: sizeOf.height * .75,
+                child: const CertificateListViewWidget(),
+              ),
+            ],
+          ),
+        ),
       ),
-       );
+    );
   }
 }
